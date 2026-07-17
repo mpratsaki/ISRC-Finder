78b05c81ca67bf6633a8a67dc113f4f34a783db1
John <110930793+mpratsaki@users.noreply.github.com>
2026-07-17 12:16:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -801,7 +801,7 @@ import pandas as pd
 
 st.set_page_config(
     page_title="Stay Independent | Catalog Generator", 
-    page_icon="🎵",
+    page_icon="StayLogo2.jpg",
     layout="wide",
     initial_sidebar_state="expanded"
 )
